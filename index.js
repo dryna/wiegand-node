c2e4b3955cd1ea4210692cb7a01fb5982831eb15
jakubdrynkowski <jacob@teacode.io>
2018-11-14 14:53:30 +0100

    [WIP] Try changing to events

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Gpio = require('onoff').Gpio;
+const EventEmitter = require('events');
 
 function Wiegand(pinD0, pinD1)
 {
@@ -11,6 +12,8 @@ function Wiegand(pinD0, pinD1)
 
     this.d0 = new Gpio(pinD0, 'in', 'falling');
     this.d1 = new Gpio(pinD1, 'in', 'falling');
+
+    EventEmitter.call(this);
 
     this.interrupts();
 }
@@ -34,6 +37,8 @@ Wiegand.prototype.ReadD0 = function()
         this._cardTemp <<= 1;        // D0 represent binary 0, so just left shift card data
     }
     this._lastWiegand = millis();    // Keep track of last wiegand bit received
+    if (this.DoWiegandConversion())
+      this._rfid_formatter(this._code);
 }
 
 Wiegand.prototype.ReadD1 = function()
@@ -52,6 +57,8 @@ Wiegand.prototype.ReadD1 = function()
         this._cardTemp <<= 1;        // left shift card data
     }
     this._lastWiegand = millis();    // Keep track of last wiegand bit received
+    if (this.DoWiegandConversion())
+      this._rfid_formatter(this._code);
 }
 
 Wiegand.prototype.noInterrupts = function()
@@ -83,10 +90,10 @@ function GetCardId(codehigh, codelow, bitlength)
     if (bitlength == 26)                              // EM tag
         return (codelow & 0x1FFFFFE) >> 1;
 
-    if (bitlength == 34)                              // Mifare 
+    if (bitlength == 34)                              // Mifare
     {
         codehigh = codehigh & 0x03;               // only need the 2 LSB of the codehigh
-        codehigh <<= 30;                           // shift 2 LSB to MSB       
+        codehigh <<= 30;                           // shift 2 LSB to MSB
         codelow >>= 1;
         return codehigh | codelow;
     }
@@ -110,26 +117,26 @@ Wiegand.prototype.DoWiegandConversion = function()
 {
     let cardID;
     const sysTick = millis();
-    
+
     if ((sysTick - this._lastWiegand) > 25)                              // if no more signal coming through after 25ms
     {
         if ((this._bitCount == 24) || (this._bitCount == 26) || (this._bitCount == 32) || (this._bitCount == 34) || (this._bitCount == 8) || (this._bitCount == 4))   // bitCount for keypress=4 or 8, Wiegand 26=24 or 26, Wiegand 34=32 or 34
         {
             this._cardTemp >>= 1;            // shift right 1 bit to get back the real value - interrupt done 1 left shift in advance
             if (this._bitCount > 32)           // bit count more than 32 bits, shift high bits right to make adjustment
-                this._cardTempHigh >>= 1;    
+                this._cardTempHigh >>= 1;
 
             if (this._bitCount == 8)       // keypress wiegand with integrity
             {
                 // 8-bit Wiegand keyboard data, high nibble is the "NOT" of low nibble
-                // eg if key 1 pressed, data=E1 in binary 11100001 , high nibble=1110 , low nibble = 0001 
+                // eg if key 1 pressed, data=E1 in binary 11100001 , high nibble=1110 , low nibble = 0001
                 const highNibble = (this._cardTemp & 0xf0) >> 4;
                 const lowNibble = (this._cardTemp & 0x0f);
-                this._wiegandType = this._bitCount;                 
+                this._wiegandType = this._bitCount;
                 this._bitCount = 0;
                 this._cardTemp = 0;
                 this._cardTempHigh = 0;
-                
+
                 if (lowNibble == (~highNibble & 0x0f))      // check if low nibble matches the "NOT" of high nibble.
                 {
                     this._code = translateEnterEscapeKeyPress(lowNibble);
@@ -165,29 +172,29 @@ Wiegand.prototype.DoWiegandConversion = function()
         {
             // well time over 25 ms and bitCount !=8 , !=26, !=34 , must be noise or nothing then.
             this._lastWiegand = sysTick;
-            this._bitCount = 0;            
+            this._bitCount = 0;
             this._cardTemp = 0;
             this._cardTempHigh = 0;
             return false;
-        }   
+        }
     }
     else
         return false;
 }
 
-function rfid_formatter(value)
+Wiegand.prototype._rfid_formatter = function (value)
 {
     let str_value = value.toString();
     while (str_value.length < 10)
         str_value = '0' + str_value;
-    return str_value;
+    this.emit('data', str_value);
 }
 
 Wiegand.prototype.available = function()
 {
-    this.noInterrupts();
+    // this.noInterrupts();
     const ret = this.DoWiegandConversion();
-    this.interrupts();
+    // this.interrupts();
     return ret;
 }
 
